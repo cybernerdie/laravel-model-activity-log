d3d83a981f6172f18d1a3c8a2bef19b0e0460ad9
cybernerdie <cybernerdie@users.noreply.github.com>
2022-09-27 16:07:37 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,9 +28,7 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-
         $migration = include __DIR__.'/../database/migrations/create_model_activity_logs_table.php.stub';
         $migration->up();
-
     }
 }
